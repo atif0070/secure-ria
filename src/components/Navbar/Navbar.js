ec76ccf51ff87bfcbdb99fdbcf8d5309b70f4b4d
atif0070 <atifbashir0070@gmail.com>
2024-03-20 19:48:06 +0500

    Add secure-ria project

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Navbar.css"
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <div className="logo">
+          <h3>SecureRia</h3>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
